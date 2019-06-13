fa5216023c2c561a9b6595a8cefa5902147df005
Sui Sui <suisuiciin@gmail.com>
2019-06-13 16:02:07 +0630

    create design form

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -11,7 +11,37 @@ Dashboard
   </div>
 
   <div class="section-body">
-  	
+     <div class="card">
+	    <!-- card header -->
+	    <div class="card-header">
+	      <!-- card title -->
+	      <h4>Stacked form</h4>
+	    </div>
+	    <!-- card body -->
+	    <div class="card-body">
+	        <div class="container">
+			  <form action="/action_page.php">
+			    <div class="form-group">
+			      <label for="email">Email:</label>
+			      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+			    </div>
+			    <div class="form-group">
+			      <label for="pwd">Password:</label>
+			      <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
+			    </div>
+			    <div class="form-group form-check">
+			      <label class="form-check-label">
+			        <input class="form-check-input" type="checkbox" name="remember"> Remember me
+			      </label>
+			    </div>
+			    <button type="submit" class="btn btn-primary">Submit</button>
+			  </form>
+			</div>
+	    </div>
+	    <!-- card footer -->
+	    <div class="card-footer">
+	    </div>
+	</div>
   </div>
 </section>
 @endsection
