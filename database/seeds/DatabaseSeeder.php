2cbd388e3a2d5d8cdab8b2d0be88186527329834
Khaing Zin <khaingzintun10@gmail.com>
2019-07-18 16:47:53 +0630

    K

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -21,4 +21,5 @@ class DatabaseSeeder extends Seeder
         $this->call(EventTypeHallsTableSeeder::class);
         $this->call(ContactTableSeeder::class);
         $this->call(AddressTableSeeder::class);
-    }   
+    }
+       
