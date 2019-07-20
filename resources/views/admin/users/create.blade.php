1b016c175a97055f0751c2ca98342438f56d6409
Htet Htet <htethteteizaw@gmail.com>
2019-07-20 14:02:13 +0630

    hall

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -8,11 +8,14 @@ Create User
 <section class="section">
   <div class="section-header">
     <h1>Add User</h1>
+        
   </div>
   <div class="section-body">
+  	<div class="card-header-action" >
+        <a href="{{route('admin.users')}}" class="btn btn-primary">UserList</a>
+        </div>
       <adduser-component></adduser-component>
   </div>
 </section>
 
-@en
-dsection
+@endsection
