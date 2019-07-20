a31f1ca3a6055baa20867f3e58309e4b514e6db0
Hnin Thu <hninthupoudel554@gmail.com>
2019-07-20 10:45:34 -0800

    j

--- resources/views/frontend/partials/menu.blade.php
+++ resources/views/frontend/partials/menu.blade.php
@@ -8,14 +8,26 @@
 			<div class="logo menu_logo">
 				<a href="#">
 					<div class="logo_container d-flex flex-row align-items-start justify-content-start">
-						<div class="logo_image"><div><img src="../frontendassets/images/logo.png" alt=""></div></div>
+						<div class="logo_image"><div><img src="../frontendassets/images/hallmyanmar.jpg" alt=""></div></div>
 					</div>
 				</a>
 			</div>
 			<ul>
 				<li class="active"><a href="homes" ><i class="fa fa-home"></i> Home</a></li>
                 <li><a href="about">About us</a></li>
-				<li><a>Properties</a></li>
+				             
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{trans('sentence.language')}}<span class="caret"></span>
+                            </a>
+                           
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="lang/en"><img src="{{asset('assets/img/us.png')}}" width="30px" height="20x"> English</a>
+                                
+                                 <a class="dropdown-item" href="lang/my"><img src="{{asset('assets/img/my.png')}}" width="30px" height="20x"> Myanmar</a>
+                                
+                            </div>
+                        </li>
 				<li><a href="contact" >Contact</a></li>
 				                <li class="upper-links dropdown show-on-hover"><a class="links dropdown-toggle" 
 									data-toggle="dropdown" href="#"><i class="fa fa-user-circle-o"></i> Account</a>
