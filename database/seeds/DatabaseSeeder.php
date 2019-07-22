d79e83192a71afc4bc0f5d4ce870ecbd5858dc7a
hnin <htetcupl1998@gmail.com>
2019-07-22 14:48:43 +0630

    t

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,15 +12,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        // $this->call(UsersTableSeeder::class);
-        // $this->call(StatesTableSeeder::class);
-        // $this->call(CitiesTableSeeder::class);
-        // $this->call(TownshipsTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(StatesTableSeeder::class);
+        $this->call(CitiesTableSeeder::class);
+        $this->call(TownshipsTableSeeder::class);
         $this->call(EventTypesTableSeeder::class);
-        // $this->call(HallsTableSeeder::class);
-        // $this->call(EventTypeHallsTableSeeder::class);
-        // $this->call(ContactTableSeeder::class);
-        // $this->call(AddressTableSeeder::class);
+        $this->call(HallsTableSeeder::class);
+        $this->call(EventTypeHallsTableSeeder::class);
+        $this->call(ContactTableSeeder::class);
+        $this->call(AddressTableSeeder::class);
 
 
         
