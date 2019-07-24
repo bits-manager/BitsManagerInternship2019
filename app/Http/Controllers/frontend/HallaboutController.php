e84b1630cef11257e5fa2c284978c120cc13d897
Wai Mi Aung <Wai Mi Aung>
2019-07-24 10:11:42 +0630

    f

--- app/Http/Controllers/frontend/HallaboutController.php
+++ app/Http/Controllers/frontend/HallaboutController.php
@@ -11,16 +11,16 @@ class HallaboutController extends Controller
     public function index(Request $request)
     {
        $hall_id= $request->hall_id;
+
        $hall=DB::table('halls')
        	      ->join('states','states.id','=','halls.state_id')
            		->join('cities','cities.id','=','halls.city_id')
            		->join('townships','townships.id','=','halls.township_id')
-           		->join('event_type_halls','event_type_halls.id','=','halls.township_id')
-           		 ->where('halls.id', '=',$hall_id)
+           		->where('halls.id', '=',$hall_id)
 
            		->select('halls.id','halls.hall_image','halls.hall_name','halls.phone_no','halls.open_time','halls.close_time','states.state_name','cities.city_name','townships.township_name','halls.address')
                 ->get();
-
+                
            $event = DB::table('event_type_halls')
                     ->join('halls', 'event_type_halls.hall_id', '=', 'halls.id')
                     ->join('event_types', 'event_types.id', '=', 'event_type_halls.eventType_id')
