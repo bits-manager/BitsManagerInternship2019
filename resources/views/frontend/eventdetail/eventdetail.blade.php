493aabca0ec3c08e7a310414bdc6ce1b7b21732e
Wai Mi Aung <Wai Mi Aung>
2019-07-29 17:02:30 +0630

    f

--- resources/views/frontend/eventdetail/eventdetail.blade.php
+++ resources/views/frontend/eventdetail/eventdetail.blade.php
@@ -2,9 +2,7 @@
 
 @section('content')
 
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  
 <style>
 
 	.section_title_service{
@@ -12,6 +10,69 @@
 	text-align: center;
 }
 </style>
+
+<!-- Header -->
+	 <header class="header">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					
+					<div class="header_content d-flex flex-row align-items-center justify-content-start">
+
+						 <div>
+							<a href="#"><img src="../frontendassets/images/hallmyanmar.png" alt="" style="width: 200px"></a>
+
+						</div> 
+						<nav class="main_nav">
+							<ul>
+								<li><a href="homes">{{trans('sentence.home')}}</a>
+								</li>
+								<li><a href="about">{{trans('sentence.about')}}</a></li>
+
+
+									<li><a href="contact">{{trans('sentence.contact')}}</a></li>
+                                  
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{trans('sentence.language')}}<span class="caret"></span>
+                            </a>
+                           
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item has-icon text-primary"  href="lang/en"><img src="{{asset('assets/img/us.png')}}" width="30px" height="20x"> English</a>
+                                
+                                 <a class="dropdown-item has-icon text-primary"  href="lang/my"><img src="{{asset('assets/img/my.png')}}" width="30px" height="20x"> Myanmar</a>
+                                
+                            </div>
+                        </li>
+                  
+								
+								<li class="upper-links dropdown show-on-hover"><a class="links dropdown-toggle" 
+									data-toggle="dropdown" href="#"> {{trans('sentence.account')}}</a>
+								  <ul class="dropdown-menu" role="menu" id="ul">
+
+								  	@if(Auth::check())
+								  	
+                                    <li>Hi, {{ Auth::user()->name }}</li>
+                                        <li><a href="{{route('admin.dashboard')}}" class="dropdown-item has-icon text-primary" >
+                                           Dashboard</a></li>
+                                        <div class="dropdown-divider"></div>
+                                        <li><a href="{{ route('logout') }}" class="dropdown-item has-icon text-danger"></i> Logout
+                                        </a></li>
+                                    @else
+                                        <li ><a href="{{route('admin.dashboard')}}" class="dropdown-item has-icon text-primary" style="color: #000"> Login</a></li>
+                                    @endif 
+          						  </ul> 
+								</li>
+							</ul>
+							
+						</nav>
+						
+						<div class="hamburger ml-auto"><i class="fa fa-bars" aria-hidden="true"></i></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
 
 <div class="about">
 		<div class="container">
@@ -21,7 +82,7 @@
 				<div class="col-lg-6">
 					<div class="about_content">
 						@foreach($data as $eventhall)
-                          <img src="{{URL::to('/')}}/images/{{$eventhall->image}}" class="img-thumbnail" width="500"/><div class="centered" >{{$eventhall->event_name}}</div>
+                          <img src="{{URL::to('/')}}/images/{{$eventhall->image}}" class="img-thumbnail" width="500"/>
                           
                          @endforeach
                         
@@ -29,9 +90,7 @@
 				</div>
 				<!-- About Content -->
                 <div class="col-lg-6">
-					<div class="about_content">
-					
-                     <div class="section_title  section_title_service" >Service</div><br>
+					<div class="about_content" style="color: #000;font-weight: 350;">
 
                         <div><b>{{$eventhall->hall_name}}</b></div>
                         <div><b>{{$eventhall->event_name}}</b></div>
